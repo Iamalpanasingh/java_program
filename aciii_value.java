ca5844924625f27301ddf4d3b6ab4fe928f4fead
Iamalpanasingh <m960348@gmail.com>
2023-02-26 15:26:40 +0530

    my second commit

---
+++ aciii_value.java
@@ -0,0 +1,12 @@
+public class aciii_value {
+    int asciivalue(char ch){
+        return ch;
+    }
+
+    public static void main(String[] args) {
+        aciii_value m=new aciii_value();
+        int ans= m.asciivalue('');
+        System.out.println("ascii value is: "+ans);
+    }
+
+}
