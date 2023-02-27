bbce48e7e276ead2e84b4504729dcb9776ea096f
Iamalpanasingh <123933135+Iamalpanasingh@users.noreply.github.com>
2023-02-27 14:00:42 +0530

    Update aciii_value.java

--- aciii_value.java
+++ aciii_value.java
@@ -5,7 +5,7 @@ public class aciii_value {
 
     public static void main(String[] args) {
         aciii_value m=new aciii_value();
-        int ans= m.asciivalue('');
+        int ans= m.asciivalue('a');
         System.out.println("ascii value is: "+ans);
     }
 
